0df7895985d8de73e29192ed8b981d6c7322e515
gedjona <edjonathan.guedes@gmail.com>
2025-03-26 23:53:49 -0300

    feat(projetoInicial): Subindo o primeiro codigo

---
+++ clientesApplication/src/test/java/edjonathan/com/br/clientesApplication/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package edjonathan.com.br.clientesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
